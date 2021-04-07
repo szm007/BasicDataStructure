2db149d4fab82a181da017c07f7c154ac8e0735d
szm007 <szm007@users.noreply.github.com>
2021-04-07 22:48:01 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# BasicDataStructure
+基本数据结构c++实现，每个文件是单独的数据结构，main函数测试
---
+++ main/main.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+#include "../BinaryTree/BST.h"
+int main()
+{
+	BST bst({ 12,3,4,2,55,6,77,98 });
+	bst.print();
+}
